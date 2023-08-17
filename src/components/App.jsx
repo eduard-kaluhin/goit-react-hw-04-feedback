27073d177aa4ea79fced8ce501d37bd6cbf31215
eduard-kaluhin <122424824+eduard-kaluhin@users.noreply.github.com>
2023-08-17 11:31:48 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -51,8 +51,8 @@ export const App = () => {
         good={good}
         neutral={neutral}
         bad={bad}
-        totalFeedback={totalFeedback}
-        positivePercentage={positivePercentage}
+        totalFeedback={totalFeedback()}
+        positivePercentage={positivePercentage()}
         feedbackOptions={options} 
           />
         )}
